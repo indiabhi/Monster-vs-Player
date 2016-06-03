ecb94f31d73195c55844ec12c391b10dea5763ff
arya <raiabhishek04@gmail.com>
2016-06-03 12:40:18 +0530

    Let user add new attr and make monster more random

--- monsters.py
+++ monsters.py
@@ -1,9 +1,24 @@
+import random
+
+COLORS = ['yellow', 'red', 'blue', 'green']
+
+
 class Monster(object):
+    min_hit_points = 1
+    max_hit_points = 1
+    min_experience = 1
+    max_experience = 1
+    weapon = "sword"
+    sound = "roar"
+
     def __init__(self, **kwargs):
-        self.hit_points = kwargs.get('hit_points', 5)
-        self.weapon = kwargs.get('weapon', 'sword')
-        self.color = kwargs.get('color', 'red')
-        self.sound = kwargs.get('sound', 'roar')
+        self.hit_points = random.randint(self.min_hit_points, self.max_hit_points)
+        self.experience = random.randint(self.min_experience, self.max_experience)
+        self.color = random.choice(COLORS)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
     def battlecry(self):
         return self.sound.upper()
